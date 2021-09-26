1a4b93ca1f8118ab8fd1ca6a24af86db196c1378
YeshuaLayme <yeshua.layme@tecsup.edu.pe>
2021-09-26 14:26:23 -0500

    lab02-2

--- lab02/lab02/urls.py
+++ lab02/lab02/urls.py
@@ -1,4 +1,4 @@
-"""lab01 URL Configuration
+"""lab02 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,15 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.http import HttpResponse
-
-def home (request):
-    return HttpResponse("<h1> HOLA MUNDO </h1>")
+from django.urls import include, path
 
 urlpatterns = [
-    path('',home),
-    path('polls/',include('encuesta.urls')),
-    path('app/',include('operaciones.urls')),
+    path('encuesta/', include('encuesta.urls')),
     path('admin/', admin.site.urls),
 ]
